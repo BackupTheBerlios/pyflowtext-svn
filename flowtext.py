63860701f27525a7656889fd3704a17888a630ab
danny <danny@d425ffc7-fe04-0410-b7cb-d563ce4bfc26>
2005-11-14 22:47:40 +0000

    Switched to using regular expressions Implemented the first basic quoted text unwrapper

--- flowtext.py
+++ flowtext.py
@@ -1,3 +1,5 @@
+import re
+
 class FormatText:
     """Use this object to perform flow operations on text
     based upon RFC2646. The unwrap routines will take text
@@ -6,44 +8,89 @@ class FormatText:
     but as it can wrap, and insert quote '>' and ' ' characters
     where necessary, it is very good for formatting text for viewing."""
     
-    def __init__(self, maxwidth = 66):
+    def __init__( self, maxwidth = 66 ):
         """The parameter maxwidth specifies the maximum width 
         for a line. Its default is 66 as this is recommmended in
         RFC 2646 that 66 character lines are most readable"""
-        self.maxwidth = maxwidth
+        self.setMaxWidth( maxwidth )
+        #Set a useful item
+        self.softnewlines = " (?:[\r\n]{1,2})(?![\r\n]{1,2})"
     
-    def getMaxWidth(self):
+    def getMaxWidth( self ):
         return self.maxwidth
     
-    def setMaxWidth(self, maxwidth):
-        if not isinstance(maxwidth, int):
-            raise TypeError("Parameter to setMaxWidth must be an integer")
+    def setMaxWidth( self, maxwidth ):
+        """The maximum width you may specify here is 79. This
+        will ensure it displays correctly even on a non-flowed program.
+        Typically, column 80 is reserved for the line-wrap indicator"""
+        if not isinstance( maxwidth, int ):
+            raise TypeError( "Parameter to setMaxWidth must be an integer" )
+        if maxwidth > 79:
+            raise ValueError( "RFC2646 Specifies a maximum of 79 characters" )
         self.maxwidth = maxwidth
     
-    def flow(self, text):
+    def flow( self, text ):
         """This is the real workhorse function,
         The parameter text contains the text to 
         be flowed"""
-        if not isinstance(text, str):
-            raise TypeError("Parameter to flow must be of type string")
+        if not isinstance( text, str ):
+            raise TypeError( "Parameter to flow must be of type string" )
         #Deal with unflowing everything, removing all soft breaks
         #unflow quoted strings (but place back in quote level)
         
         #Reflow each line
         
-    def unwrap(self, text):
+    def unwrapquoted( self, text ):
+        """This will unwrap the text, dealing specifically with quoted stuff
+        and try to keep paragraphs together"""
+        #quotematch = "(?:(?:[> ]+)|(?:From ))"
+        quotematch = "(?:[> ]+)"
+        expression = "^(?P<quote>" + quotematch + ")(?P<text>.*?)" + self.softnewlines
+        #Add the second line - repeated quote then text that is NOT 
+        #the quote - note we use (?!...) for negative lookahead
+        expression += "(?:(?P=quote))"
+        #?" + "(?!" + quotematch + ")"
+        replace = "\g<quote>\g<text> "
+        return re.sub( string = text, pattern=expression, repl= replace)
+
+        
+    def unwrap( self, text ):
         """This will unwrap the text and try to keep paragraphs together"""
-        whitespace = False
-        newchars = ''
-        for char in text:
-            if not (whitespace == True and char == '\n'):
-                newchars += char
-            if char == ' ' or char == '\t':
-                whitespace = True
-            else:
-                whitespace = False
-                
-        return newchars
+        expression = self.softnewlines
+        replace = " "
+        return re.sub( string = text, pattern=expression, repl= replace)
+
+    
+#    def unwrap(self, text):
+#        """This will unwrap the text and try to keep paragraphs together"""
+#        whitespace = False
+#        newtext = ''
+#        knownquotes = ['From ','>',' ']
+#        quotechars = ''
+#        oldquotechars = ''
+#        newpara = True
+#        newline = True
+#        for char in text:
+#            if char == '\n':
+#                newline = True
+#                oldquotechars = quotechars;
+#                quotechars = ''
+#                
+#            if(newline and not whitespace):
+#                newpara = True
+#                oldquotechars = ''                
+#            if not (whitespace and newline):
+#                newtext += char
+#                
+#            #Handle quoting
+#            if
+#            #Handle whitespace
+#            if char == ' ' or char == '\t':
+#                whitespace = True
+#            else:
+#                whitespace = False
+#                
+#        return newtext
                 
                 
                 
--- flowtexttests.py
+++ flowtexttests.py
@@ -1,66 +1,133 @@
-import unittest
 import flowtext
+import unittest
 
-class ReflowTestCase(unittest.TestCase):
-    def setUp(self):
+class ReflowTestCase( unittest.TestCase ):
+    def setUp( self ):
         pass
     
-    def testConstruction(self):
-        """This tests the default constuctor"""
+    def testConstruction( self ):
         flow = flowtext.FormatText()
-        self.assertEqual(66, flow.getMaxWidth())
+        self.assertEqual( 66, flow.getMaxWidth() )
+        #Try constructing again
+        flow = flowtext.FormatText( 23 )
+        self.assertEqual( 23, flow.getMaxWidth() )
+    
+    def testSetMaxWidth( self ):
+        flow = flowtext.FormatText()
+        flow.setMaxWidth( 40 )
+        self.assertEqual( 40, flow.getMaxWidth() )
+        def setOverWide():
+            flow.setMaxWidth( 99 )
+        self.assertRaises( ValueError, setOverWide )
         
-    def testUnwrap(self):
+    def testUnwrap( self ):
         flow = flowtext.FormatText()
-        testText = ("This line spans a few \n" +
+        testText = ( "This line spans a few \n" +
             "lines and has a couple of soft \n" +
-            "breaks in it. ")
-        expectedText = ("This line spans a few " + 
+            "breaks in it. " )
+        expectedText = ( "This line spans a few " + 
             "lines and has a couple of soft " +
-            "breaks in it. ")
-        self.assertEqual(expectedText, flow.unwrap(testText))
-        
-    def testUnwrapTwoParagraphs(self):
+            "breaks in it. " )
+        self.assertEqual( expectedText, flow.unwrap( testText ) )
+
+    def testNegativeUnwrap( self ):
         flow = flowtext.FormatText()
-        testText = ("This line spans a few \n" +
+        testText = ( "This line spans a few \n" +
+            "lines and has a couple of soft \n" +
+            "breaks in it. " )
+        self.assertNotEqual( testText, flow.unwrap( testText ) )    
+    
+    def testUnwrapTwoParagraphs( self ):
+        flow = flowtext.FormatText()
+        testText = ( "This line spans a few \n" +
             "lines and has a couple of soft \n" +
             "breaks in it.\n\n" + 
             "and sneakily has a second \n" + 
-            "paragraph")
-        expectedText = ("This line spans a few " + 
+            "paragraph" )
+        expectedText = ( "This line spans a few " + 
             "lines and has a couple of soft " +
             "breaks in it.\n\n" +
             "and sneakily has a second " + 
-            "paragraph")
+            "paragraph" )
 
-        self.assertEqual(expectedText, flow.unwrap(testText))
-
-    """
-    def testUnwrapQuoted(self):
-        pass;
-    """
+        self.assertEqual( expectedText, flow.unwrap( testText ) )
+                
     
-    """
-    def testFlow(self):
+    def testUnwrapQuotedWithSpace( self ):
+        flow = flowtext.FormatText()
+        testText = ( " This line is \n" +
+            " quoted and has a couple of soft \n" +
+            " breaks in it." )
+        expectedText = ( " This line is " +
+            "quoted and has a couple of soft " +
+            "breaks in it." )
+
+        self.assertEqual( expectedText, flow.unwrapquoted( testText ) )
+
+    def testUnwrapQuotedWithGt( self ):
+        flow = flowtext.FormatText()
+        testText = ( ">This line is \n" +
+            ">quoted and has a couple of soft \n" +
+            ">breaks in it." )
+        expectedText = ( ">This line is " +
+            "quoted and has a couple of soft " +
+            "breaks in it." )
+
+        self.assertEqual( expectedText, flow.unwrapquoted( testText ) )
+
+    def testASimpleUnwrapQuotedWithGt( self ):
+        flow = flowtext.FormatText()
+        testText = ( ">This line is \n>quoted")
+        expectedText = ( ">This line is quoted")
+
+        self.assertEqual( expectedText, flow.unwrapquoted( testText ) )    
+    
+    def testUnwrapMixedQuoted( self ):
+        flow = flowtext.FormatText()
+        testText = ( ">This line is \n" +
+            ">quoted and has a couple of soft \n" +
+            ">breaks in it.\n\n" +
+            " This line is also \n" + 
+            " quoted" )
+        expectedText = ( ">This line is " +
+            "quoted and has a couple of soft " +
+            "breaks in it.\n\n" +
+            " This line is also " + 
+            "quoted" )
+
+        self.assertEqual( expectedText, flow.unwrap( testText ) )
+    
+    def testUnwrapAll(self):
+        flow = flowtext.FormatText()
         testText = ("This line spans a few \n" + 
             "lines and has a couple of soft \n" +
-            "breaks in it. It also has a very, very long line which spans a little too long and therefore needs to be well wrapped.\n"
-            )
-        #We are going with the exected output from the unwrap
-    """
-            
-    """             + 
-            "\nAfter which there is a new paragraph. This new paragraph has only one long line\n" +
+            "breaks in it. It also has a very, very long line which spans a little " +
+            "too long and therefore needs to be well wrapped.\n" + 
+            "\nAfter which there is a new paragraph. This new paragraph has only " + 
+            "one long line\n" +
             " And now an indented (therefore quoted) section \n" +
-            " which also spans a few lines, with soft line breaks (using the space then crlf convention)\n" +
+            " which also spans a few lines, with soft line breaks (using the space " +
+            "then crlf convention)\n" +
             "\n>Another quoted test using the greater than sign \n" +
             ">but its otherwise nothing special.\n\n" +
             ">And here I am going to give a soft break \n" +
-            ">>And change the quoting depth - this is an illegal operation"
-   """
-        
-    
-testSuite = unittest.TestSuite((unittest.makeSuite(ReflowTestCase)))
+            ">>And change the quoting depth - this is an illegal operation")
+        expectedText = ("This line spans a few " + 
+            "lines and has a couple of soft " +
+            "breaks in it. It also has a very, very long line which spans a little " +
+            "too long and therefore needs to be well wrapped.\n" + 
+            "\nAfter which there is a new paragraph. This new paragraph has only " + 
+            "one long line\n" +
+            " And now an indented (therefore quoted) section" +
+            " which also spans a few lines, with soft line breaks (using the space " +
+            "then crlf convention)\n" +
+            "\n>Another quoted test using the greater than sign " +
+            "but its otherwise nothing special.\n\n" +
+            ">And here I am going to give a soft break \n" +
+            ">>And change the quoting depth - this is an illegal operation")
+        self.assertEqual( expectedText, flow.unwrap( testText ) )
 
-runner = unittest.TextTestRunner(verbosity = 2)
-runner.run(testSuite)
+testSuite = unittest.TestSuite( ( unittest.makeSuite( ReflowTestCase ) ) )
+
+runner = unittest.TextTestRunner( verbosity = 2 )
+runner.run( testSuite )
